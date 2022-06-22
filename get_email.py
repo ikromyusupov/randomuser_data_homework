8c3472c01e603254b20ace68253a2b7a2d39c773
Ikrom <ikromyusupov09@gmail.com>
2022-06-22 18:25:58 +0500

    users email

--- get_email.py
+++ get_email.py
@@ -1,11 +1,19 @@
 import get_data
 
-def get_email(data:dict) -> list:
+
+def get_email(data: dict) -> list:
     """
     Take the email of the users and return the list.
-
     Args:
         data(dict): data
     Returns:
         list: users email
     """
+    emails = []
+    for email in data['results']:
+        emails.append(email['email'])
+    return emails
+
+
+data = get_data.get_data('randomuser_data.json')
+print(get_email(data))
